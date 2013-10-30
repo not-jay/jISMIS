1c65965882c37687abbe2315d961c7c56e054700
xTouchMe <jard@that-one-guy.tk>
2013-10-30 22:33:40 +0800

    Fixed student data parsing/constructor

--- src/com/xtouchme/ismis/data/Student.java
+++ src/com/xtouchme/ismis/data/Student.java
@@ -20,18 +20,24 @@ public class Student {
 		
 		this.idNumber = data[0].split(":")[1];
 		this.name = data[1].split(":")[1];
-		this.prospectus = data[2].split(":")[1];
-		this.yearLevel = Integer.parseInt(data[3].split(":")[1]);
+		
+		//Only happens when Student Prospectus is empty
+		//Which only happens on old? accounts
+		if(!ismisData.contains("Student Prospectus:Year Level")) {
+			this.prospectus = data[2].split(":")[1];
+			this.yearLevel = Integer.parseInt(data[3].split(":")[1]);
+		} else
+			prospectus = "";
 		
 		//Properly capitalize the name, ALL CAPS is for cruise control lol
-		String tempName[] = this.name.split(" ");
+		String tempName[] = this.name.replaceAll("^ +| +$|( )+", "$1").split(" "); //Pheex double spaces
 		this.name = "";
 		for(String s : tempName) {
+			s = s.replaceAll("&#209;", "N~"); //Temporarily replace
 			s = s.substring(0, 1)+s.substring(1).toLowerCase();
 			this.name += " "+s.trim();
 		}
-		this.name = this.name.substring(1);
-		
+		this.name = this.name.substring(1).replaceAll("n~", "ñ").replaceAll("N~", "Ñ"); //Fix "Ñ and ñ"s
 	}
 	
 	public String getIsmisID() {
@@ -60,6 +66,8 @@ public class Student {
 			year = String.format("%dth", yearLevel);
 		}
 		year += " year";
+		
+		if(yearLevel == 0) year = "";
 		
 		return String.format("[%s] %s%n%s %s", idNumber, name, prospectus, year);
 	}
