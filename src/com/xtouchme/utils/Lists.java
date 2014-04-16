d28309d84fff5675760f4ade350eab14181f5b09
xTouchMe <jardsonsona@yandex.com>
2014-04-16 20:53:09 +0800

    apache commons-lang + Lists util class

---
+++ src/com/xtouchme/utils/Lists.java
@@ -0,0 +1,33 @@
+package com.xtouchme.utils;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lists {
+
+	/**
+	 * Compares two lists and returns a new list containing elements not found in the other list
+	 * @param a List A
+	 * @param b List B
+	 * @return null, if both lists are empty or null<br />
+	 * 		   A,	 if b is empty or null<br />
+	 * 		   B,	 if a is empty or null<br />
+	 * 		   a new List containing both A and B's unique elements, otherwise
+	 */
+	public static <T> List<T> diff(List<T> a, List<T> b) {
+		if((a == null && b == null) || (a.isEmpty() && b.isEmpty())) return null;
+		if(a == null || a.isEmpty()) return b;
+		if(b == null || b.isEmpty()) return a;
+		
+		List<T> diff = new LinkedList<>();
+		for(T element : a) {
+			if(!b.contains(element)) diff.add(element);
+		}
+		for(T element : b) {
+			if(!a.contains(element)) diff.add(element);
+		}
+		
+		return diff;
+	}
+	
+}
