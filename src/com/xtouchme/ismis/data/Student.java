8c5437b4c711d4acecd01855d89d5c577152ce8a
xTouchMe <jardsonsona@yandex.com>
2014-04-16 18:15:52 +0800

    Prepared for code changes

--- src/com/xtouchme/ismis/data/Student.java
+++ src/com/xtouchme/ismis/data/Student.java
@@ -1,20 +1,26 @@
 package com.xtouchme.ismis.data;
 
+import org.apache.commons.lang3.text.WordUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Student {
 
-	private String studentID;
+	private String internalId;
 	private String idNumber;
 	private String name;
 	private String prospectus;
 	private int yearLevel;
 	
 	/**
+	 * TODO: Pending removal
+	 * 
 	 * This should only be instantiated within the com.xtouchme.ismis.Ismis class
 	 * 
 	 * @param ismisData - '\r\n' separated key:value pair data of the student
 	 */
 	public Student(String studentID, String ismisData) {
-		this.studentID = studentID;
+		this.internalId = studentID;
 		
 		String data[] = ismisData.split("\r\n");
 		
@@ -40,11 +46,28 @@ public class Student {
 		this.name = this.name.substring(1).replaceAll("n~", "ñ").replaceAll("N~", "Ñ"); //Fix "Ñ and ñ"s
 	}
 	
-	public String getIsmisID() {
-		return studentID;
+	/**
+	 * Creates a student object using the details from a JSONobject
+	 * @param data
+	 * @throws JSONException if data is missing a key
+	 */
+	public Student(JSONObject data) {
+		try {
+			this.internalId	= data.getString("Internal Id");
+			this.idNumber	= data.getString("Student Id");
+			this.name		= WordUtils.capitalizeFully(data.getString("Student Name"));
+			this.prospectus	= data.getString("Student Prospectus");
+			this.yearLevel	= data.getInt("Year Level");
+		} catch(JSONException e) {
+			System.err.printf("Malformed data: %s%n", e.getMessage());
+		}
+	}
+
+	public String getInternalId() {
+		return internalId;
 	}
 	
-	public String getIDNumber() {
+	public String getIdNumber() {
 		return idNumber;
 	}
 	
@@ -56,18 +79,12 @@ public class Student {
 		String year = "";
 		
 		switch(yearLevel) {
-		case 1:
-			year = "1st"; break;
-		case 2:
-			year = "2nd"; break;
-		case 3:
-			year = "3rd"; break;
-		case 4:
-			year = "4th"; break;
-		case 5:
-			year = "5th"; break;
-		default:
-			year = String.format("%dth", yearLevel);
+		case 1:	year = "1st"; break;
+		case 2: year = "2nd"; break;
+		case 3: year = "3rd"; break;
+		case 4: year = "4th"; break;
+		case 5: year = "5th"; break;
+		default: year = String.format("%dth", yearLevel);
 		}
 		year += " year";
 		
@@ -86,7 +103,7 @@ public class Student {
 	
 	@Override
 	public String toString() {
-		return String.format("[%s] %s%n%s %s", idNumber, name, prospectus, getYearLevelString());
+		return String.format("[%s] %s %s %s", idNumber, name, prospectus, getYearLevelString());
 	}
 	
 }
